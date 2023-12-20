bda4b914f7b639222d8b09befaa607bf08bcf197
a85241 <148544628+a85241@users.noreply.github.com>
2023-12-20 11:00:05 +0000

    Update docusaurus.config.js
    Reparação e Manutenção do seu Veículo !!!

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Site da Norauto - Rodrigo Neves e Daniel Pinto',
-  tagline: 'Dinosaurs are cool!',
+  tagline: 'Reparação e Manutenção do seu Veículo !!!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
