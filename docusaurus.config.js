e328dd6e770a669eda7581c6256cc9c7269567e1
a85241 <148544628+a85241@users.noreply.github.com>
2024-01-07 14:59:21 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Norauto',
-  tagline: 'Reparação e Manutenção do seu Veículo !!!',
+  tagline: 'Reparação e Manutenção do seu Veículo !',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
