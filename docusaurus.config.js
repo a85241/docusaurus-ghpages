65686b2a024828b944460e655931f8dd97cd3682
a85241 <148544628+a85241@users.noreply.github.com>
2024-01-07 15:03:57 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -128,7 +128,6 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
